25f8e62815e379d21062e20a417e4788c0fc6d4e
lim <tbnsok40@naver.com>
2020-06-26 16:17:47 +0900

    function init()

--- DOM Manipulation and Event/index.js
+++ DOM Manipulation and Event/index.js
@@ -1,86 +1,112 @@
-var scores, roundScore, activePlayer; // this is global scope
+// var scores, roundScore, activePlayer; // this is global scope
 
-init();
+// init();
 
-document.querySelector('.btn-roll').addEventListener('click', function () {
-    // 1. Random number
-    var dice = Math.floor(Math.random() * 6) + 1;
+// document.querySelector('.btn-roll').addEventListener('click', function () {
+//     // 1. Random number
+//     var dice = Math.floor(Math.random() * 6) + 1;
 
-    //2. Display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + dice + '.png';
-
-
-    //3. Update the round score IF the rolled number was NOT a 1
-    if (dice !== 1) {
-        //Add score
-        roundScore += dice;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        //Next player
-        nextPlayer();
-    }
-}
-);
-
-document.querySelector('.btn-hold').addEventListener('click', function () {
-    // add current score to global score
-    scores[activePlayer] += roundScore;
-
-    // Update the UI
-    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-
-    // check if the player won the game
-    if (scores[activePlayer] >= 20) {
-        document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-        document.querySelector('.dice').style.display = 'none';
-        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); // what the hell is classList
-        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); // add, remove, toggle
-    } else {
-        nextPlayer();
-    }
-});
-
-function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // 삼항 연산자(조건문),sipler than if statements
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
-}
+//     //2. Display the result
+//     var diceDOM = document.querySelector('.dice');
+//     diceDOM.style.display = 'block';
+//     diceDOM.src = 'dice-' + dice + '.png';
 
 
-document.querySelector('.btn-new').addEventListener('click', init); // not calling it, just passing it (function init())
+//     //3. Update the round score IF the rolled number was NOT a 1
+//     if (dice !== 1) {
+//         //Add score
+//         roundScore += dice;
+//         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+//     } else {
+//         //Next player
+//         nextPlayer();
+//     }
+// }
+// );
 
+// document.querySelector('.btn-hold').addEventListener('click', function () {
+//     // add current score to global score
+//     scores[activePlayer] += roundScore;
+
+//     // Update the UI
+//     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+//     // check if the player won the game
+//     if (scores[activePlayer] >= 20) {
+//         document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+//         document.querySelector('.dice').style.display = 'none';
+//         document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner'); // what the hell is classList
+//         document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active'); // add, remove, toggle
+//     } else {
+//         nextPlayer();
+//     }
+// });
+
+// function nextPlayer() {
+//     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // 삼항 연산자(조건문),sipler than if statements
+//     roundScore = 0;
+
+//     document.getElementById('current-0').textContent = '0';
+//     document.getElementById('current-1').textContent = '0';
+
+//     document.querySelector('.player-0-panel').classList.toggle('active');
+//     document.querySelector('.player-1-panel').classList.toggle('active');
+
+//     document.querySelector('.dice').style.display = 'none';
+// }
+
+
+// document.querySelector('.btn-new').addEventListener('click', init); // not calling it, just passing it (function init())
+
+// function init() {
+
+//     scores = [0, 0];
+//     roundScore = 0;
+//     activePlayer = 0;
+
+//     document.querySelector('.dice').style.display = 'none'; // default는 안 보이도록 setting(주사위)
+
+
+//     document.getElementById('score-0').textContent = '0';
+//     document.getElementById('score-1').textContent = '0';
+//     document.getElementById('current-0').textContent = '0';
+//     document.getElementById('current-1').textContent = '0'; //html의 text를 직접 셋팅
+
+//     document.getElementById('name-0').textContent = 'Player 1';
+//     document.getElementById('name-1').textContent = 'Player 2';
+//     document.querySelector('.player-0-panel').classList.remove('winner');
+//     document.querySelector('.player-1-panel').classList.remove('winner');
+//     document.querySelector('.player-0-panel').classList.remove('active');
+//     document.querySelector('.player-1-panel').classList.remove('active');
+//     document.querySelector('.player-0-panel').classList.add('active'); 
+
+
+// }
+// // textContent의 c가 소문자가 아니도록 주의하자 시벌
+// // getElementsById와 getElementById는 다를다. s 주의 **
+
+
+
+var scores, roundScore, activePlayer;
 function init() {
-
     scores = [0, 0];
     roundScore = 0;
     activePlayer = 0;
 
-    document.querySelector('.dice').style.display = 'none'; // default는 안 보이도록 setting(주사위)
-
+    document.querySelector('.dice').style.display = 'none';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0'; //html의 text를 직접 셋팅
+    document.getElementById('current-1').textContent = '0';
 
-    document.getElementById('name-0').textContent = 'Player 1';
-    document.getElementById('name-1').textContent = 'Player 2';
-    document.querySelector('.player-0-panel').classList.remove('winner'); // what the hell is classList
-    document.querySelector('.player-1-panel').classList.remove('winner'); // what the hell is classList
-    document.querySelector('.player-0-panel').classList.remove('active'); // what the hell is classList
-    document.querySelector('.player-1-panel').classList.remove('active'); // what the hell is classList
-    document.querySelector('.player-0-panel').classList.add('active'); // what the hell is classList
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
 
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
 
+    document.querySelector('.player-0-panel').classList.add('active');
 }
-// textContent의 c가 소문자가 아니도록 주의하자 시벌
-// getElementsById와 getElementById는 다를다. s 주의 **
