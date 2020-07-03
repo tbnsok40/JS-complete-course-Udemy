4ed274dcdffdb4f052a1c9c5b61443d0f8011565
lim <tbnsok40@naver.com>
2020-07-03 23:43:51 +0900

    IFFE

--- Objects and functions/index.js
+++ Objects and functions/index.js
@@ -181,3 +181,15 @@ interviewQuestion('developer')('LIM'); // this is awesome.
 console.log()
 
 
+//IFFE: for data privacy
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+// console.log(score);
+
+(function (goodluck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodluck);
+})(5);
