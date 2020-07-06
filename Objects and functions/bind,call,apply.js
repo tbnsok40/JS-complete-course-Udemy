df535cf275c5e8df898a7e8a9002460df7638ae7
lim <tbnsok40@naver.com>
2020-07-07 00:52:02 +0900

    call,apply,bind

--- Objects and functions/bind,call,apply.js
+++ Objects and functions/bind,call,apply.js
@@ -21,15 +21,18 @@ var emily = {
 
 john.presentation('formal', 'morning');
 
-// call method, using this method.
-john.presentation.call(emily, 'friendly', 'afternoon');
+//// call method, using this method.////
+john.presentation.call(emily, 'friendly', 'afternoon'); // calling emily object
+john.presentation.call(emily, 'friendly', 'Goodmorning');
 
 // apply method
 // john.presentation.apply(emily, )
 
-// bind method, doenst call the function immediately, but generate it lately
+//// bind method, doenst call the function immediately, but generate it lately ////
+// so call and bind is 반대
 var johnFriendly = john.presentation.bind(john, 'friendly');
 // function get stored in variable johnFriendly
+// 왜 friendly 인자 칸만 가능하지? 'morning'칸은 왜 불가?
 
 johnFriendly('morningggg');
 johnFriendly('Good eveningg');
@@ -37,7 +40,11 @@ johnFriendly('Good eveningg');
 var emilyFormal = john.presentation.bind(emily, 'formal');
 emilyFormal('afternoon');
 
-var years = [1990, 1965, 1937, 1993, 2000];
+
+
+/////////////////new thang/////////////////
+var years = [2001, 1965, 1937, 1993, 2000];
+// 함수안에 함수 넣는 것은 이름이 뭐여
 function arrayCalc(arr, fn) {
     var arrRes = [];
     for (var i = 0; i < arr.length; i++) {
@@ -50,12 +57,27 @@ function arrayCalc(arr, fn) {
 function calculateAge(element) {
     return 2016 - element;
 }
-
 function isFullAge(limit, element) {
     return element >= limit;
 }
 var ages = arrayCalc(years, calculateAge);
 
+// bind는 함수가 가르키는 this만 바꾸고 호출하지 않는 것.
 var fullKR = arrayCalc(ages, isFullAge.bind(this, 20)) // we can preset the argument(function isFullAge)
 console.log(ages);
 console.log(fullKR);
+
+
+var lim = {
+    string: 'zero',
+    yell: function () {
+        alert(this.string);
+    }
+};
+
+var kim = {
+    string: '2?'
+};
+
+var yell2 = lim.yell.call(kim);
+// yell2();
--- Objects and functions/memo.js
+++ Objects and functions/memo.js
@@ -37,3 +37,22 @@
 // }
 // interviewQuestion('developer')('LIM');
 
+var years = [2001, 1965, 1937, 1993, 2000];
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]))
+    }
+    return arrRes;
+}
+
+function calculateAge(element) {
+    return 2020 - element;
+}
+function isFullAge(limit, element) {
+    return element >= limit;
+}
+var ages = arrayCalc(years, calculateAge);
+var fullKR = arrayCalc(ages, isFullAge.bind(this, 20))
+console.log(ages)
+console.log(fullKR)
