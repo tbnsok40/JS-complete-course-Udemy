5a8ed6511b0bad5a0a45d4ba230f405cb1afbd84
Lim <40200760+tbnsok40@users.noreply.github.com>
2021-04-29 15:28:14 +0900

    [fin] dom-array project

--- 20 toy projects/5. Dom-array/script.js
+++ 20 toy projects/5. Dom-array/script.js
@@ -7,14 +7,9 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 
 let data = []; //global 변수
 
-
-
 getRandomUser();
 getRandomUser();
 getRandomUser();
-
-
-
 
 
 // fetch random user and add Money
@@ -23,50 +18,38 @@ async function getRandomUser() {
     // use async then we don need this chain stuffs
     // fetch('https://randomuser.me/api').then(res => res.json()).then(data => )
 
-
     // fetch is asynchronous => so use the await
     const res = await fetch('https://randomuser.me/api');
     const data = await res.json();
-    console.log(data)
-
     const user = data.results[0];
-
     const newUser = {
         name: `${user.name.first} ${user.name.last}`,
         money: Math.floor(Math.random() * 1000000)
 
     }
-
     addData(newUser) // newUser가 data가 된다. 놀래라이씨
-
 }
 
 function doubleMoney() {
-
     data.map(user => {
         console.log(user)
     })
     // 전역변수 data를 가져다 mapping
     data = data.map((user) => {
         // spread operation
-        return { ...user, money: user.money * 2 }
+        return {...user, money: user.money * 2}
         // spread연산자로 user의 기존 정보들은 모두 그대로 박아두고, money라는 key에 대해서만 값을 수정하여 return한다.
     })
-
     updateDom();
 }
-
-
 
 
 // Add new obj to data array
 
 function addData(obj) {
     data.push(obj)
-
     updateDom(); // if nothing is in the args, then just thorow the data as args (originally it supposed to get the providedData)
 }
-
 
 
 // Updata Dom
@@ -88,17 +71,30 @@ function updateDom(providedData = data) {
 
 // Format Money
 function formatMoney(number) {
-
     // ref stackoverflow
     return "$ " + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
-
-
 }
 
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+    updateDom();
+}
 
+function showMillionaires() {
+    data = data.filter(user => user.money > 300000);
+    updateDom();
+}
+
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealthEl = document.createElement("div");
+    wealthEl.innerHTML = `<h3> Total wealth: ${wealth}</h3>`
+    main.appendChild(wealthEl)
+}
 
 // single arrow function doesn't need return 
-
-
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth)
